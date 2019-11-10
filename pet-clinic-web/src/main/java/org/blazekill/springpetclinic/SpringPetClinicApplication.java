0bfe5256c02e0db2be0fc22ec31d5dfa7241d2d1
Devashish Lal <laldevashish@gmail.com>
2019-11-10 18:17:59 +0530

    Index controller added

--- pet-clinic-web/src/main/java/org/blazekill/springpetclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/org/blazekill/springpetclinic/SpringPetClinicApplication.java
@@ -1,13 +1,18 @@
 package org.blazekill.springpetclinic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringPetClinicApplication {
 
+    public static final Logger logger = LoggerFactory.getLogger(SpringPetClinicApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringPetClinicApplication.class, args);
+        logger.info("Listening on http://localhost:3200");
     }
 
 }
