c5b0dcceea1f4acf29d964bc0d1213958da251bd
Devashish Lal <laldevashish@gmail.com>
2019-11-10 07:45:10 +0530

    Crud service abstraction created

--- pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/PetService.java
@@ -2,14 +2,6 @@ package org.blazekill.springpetclinic.services;
 
 import org.blazekill.springpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package org.blazekill.springpetclinic.services;
 
 import org.blazekill.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
