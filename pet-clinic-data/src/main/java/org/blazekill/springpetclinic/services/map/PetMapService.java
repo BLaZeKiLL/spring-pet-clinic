7215338a5f5fd1acbf5f91c9b261ec7d966fe144
Devashish Lal <laldevashish@gmail.com>
2019-11-10 17:52:26 +0530

    Map services created

---
+++ pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/map/PetMapService.java
@@ -0,0 +1,35 @@
+package org.blazekill.springpetclinic.services.map;
+
+import org.blazekill.springpetclinic.model.Pet;
+import org.blazekill.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
