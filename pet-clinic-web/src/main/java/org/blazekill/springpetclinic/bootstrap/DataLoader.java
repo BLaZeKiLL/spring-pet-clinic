1b3e1f7b77dd02d0c1b11cc6974d9ab41715e5fb
Devashish Lal <laldevashish@gmail.com>
2019-11-27 08:13:15 +0530

    Pet type added to data loader

--- pet-clinic-web/src/main/java/org/blazekill/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/blazekill/springpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package org.blazekill.springpetclinic.bootstrap;
 
 import org.blazekill.springpetclinic.model.Owner;
+import org.blazekill.springpetclinic.model.PetType;
 import org.blazekill.springpetclinic.model.Vet;
 import org.blazekill.springpetclinic.services.OwnerService;
+import org.blazekill.springpetclinic.services.PetTypeService;
 import org.blazekill.springpetclinic.services.VetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +19,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        logger.info("Loaded PetTypes...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Devashish");
         owner1.setLastName("Lal");
