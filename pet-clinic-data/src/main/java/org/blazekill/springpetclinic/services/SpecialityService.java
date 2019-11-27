64ee8aeed75f91397fededa5161532ae2a343879
Devashish Lal <laldevashish@gmail.com>
2019-11-27 08:33:35 +0530

    Speciality Service and map implementation created

---
+++ pet-clinic-data/src/main/java/org/blazekill/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package org.blazekill.springpetclinic.services;
+
+import org.blazekill.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
